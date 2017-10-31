f266a2c6ff723beae14770be473c042c5d6fce13
Juan Sebastian Navarro <jsnavarroa@unal.edu.co>
2017-10-31 16:09:55 -0500

    Prepare code for different OpenCL kernels

--- Exercise02/C/main.cpp
+++ Exercise02/C/main.cpp
@@ -15,29 +15,31 @@
 using namespace std;
 using namespace boost::timer;
 
-void sequential(float* a, float* b, float* c) {
-	mymultiply(a, b, c);
-}
-
-void opencl(float* a, float* b, float* c) {
-	myopencl(a, b, c);
-}
-
-void printLog(long matrix_size, cpu_timer s_timer, cpu_timer o_timer,
+void printLog(long matrix_size, cpu_timer s_timer, cpu_timer o_timer, cpu_timer ocl_tv_timer, cpu_timer ocl_shm_timer,
 		bool test) {
-	string s_time = s_timer.format(3, "%ws");
-	string o_time = o_timer.format(3, "%ws");
+	string sequential = s_timer.format(3, "%ws");
+	string ocl_simple = o_timer.format(3, "%ws");
+	string ocl_tv = ocl_tv_timer.format(3, "%ws");
+	string ocl_shm = ocl_shm_timer.format(3, "%ws");
 	string result = test ? "Passed" : "Failed";
-	printf("%ld \t %s \t %s \t %s \n", matrix_size, s_time.c_str(),
-			o_time.c_str(), result.c_str());
+
+	printf("%ld \t %s \t %s \t", matrix_size, sequential.c_str(),
+			ocl_simple.c_str());
+	printf("%s \t", ocl_tv.c_str());
+	printf("%s \t", ocl_shm.c_str());
+
+	printf("%s \t", result.c_str());
+	printf("\n");
 }
 
 int main(int argc, char *argv[]) {
 
 	M_LENGTH = 256;
 
+	bool result = true;
+
 	cpu_timer sequential_timer;
-	cpu_timer opencl_timer;
+	cpu_timer opencl_timer, ocl_tv_timer, ocl_shm_timer;
 
 	float* h_a = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float)); // a vector
 	float* h_b = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float)); // b vector
@@ -53,22 +55,35 @@ int main(int argc, char *argv[]) {
 
 	sequential_timer.start();
 
-	sequential(h_a, h_b, h_s);
+	mymultiply(h_a, h_b, h_s);
 
 	sequential_timer.stop();
 	//printMatrix((char *) "Sequential", h_s);
 
 	opencl_timer.start();
-
-	opencl(h_a, h_b, h_c);
-
+	myopencl_simple(h_a, h_b, h_c);
 	opencl_timer.stop();
 	//printMatrix((char *) "OpenCL", h_c);
 
-	bool result = equal(h_s, h_c);
-	printf("Test: %s \n", (result ? "Passed" : "Failed"));
+	result = result && equal(h_s, h_c);
 
-	printLog(M_LENGTH, sequential_timer, opencl_timer, result);
+	// OPENCL WITH TEMP VAR
+	ocl_tv_timer.start();
+	myopencl_simple(h_a, h_b, h_c);
+	ocl_tv_timer.stop();
+
+	result = result && equal(h_s, h_c);
+
+	// OPENCL WITH shared memory
+	ocl_shm_timer.start();
+	myopencl_shared_memory(h_a, h_b, h_c);
+	ocl_shm_timer.stop();
+
+	result = result && equal(h_s, h_c);
+
+	printf("matrix_size \t sequential \t ocl_simple \t ocl_temp_var \t ocl_shared_mem \t result \n");
+	printLog(M_LENGTH, sequential_timer, opencl_timer, ocl_tv_timer,
+			ocl_shm_timer, result);
 
 	// FREE memory
 
--- Exercise02/C/mopencl.h
+++ Exercise02/C/mopencl.h
@@ -46,7 +46,43 @@ extern int output_device_info(cl_device_id);
 // output: c float vector of length count holding the sum a + b
 //
 
-const char *KernelSource =
+const char *KernelSource_simple =
+		"\n"
+				"__kernel void mymultiply(                                                 \n"
+				"   __global float* a,                                                  \n"
+				"   __global float* b,                                                  \n"
+				"   __global float* c,                                                  \n"
+				"   const unsigned int count)                                           \n"
+				"{                                                                      \n"
+				"   for (int i = 0; i < count; i++) {           \n"
+				"   	for (int j = 0; j < count; j++) {       \n"
+				"   		for (int k = 0; k < count; k++) {   \n"
+				"   			c[i * count + j] += a[i * count + k] * b[k * count + j];    \n"
+				"   		}                                    \n"
+				"   	}                                        \n"
+				"   }                                            \n"
+				"}                                                                      \n"
+				"\n";
+
+const char *KernelSource_temp_var =
+		"\n"
+				"__kernel void mymultiply(                                                 \n"
+				"   __global float* a,                                                  \n"
+				"   __global float* b,                                                  \n"
+				"   __global float* c,                                                  \n"
+				"   const unsigned int count)                                           \n"
+				"{                                                                      \n"
+				"   for (int i = 0; i < count; i++) {           \n"
+				"   	for (int j = 0; j < count; j++) {       \n"
+				"   		for (int k = 0; k < count; k++) {   \n"
+				"   			c[i * count + j] += a[i * count + k] * b[k * count + j];    \n"
+				"   		}                                    \n"
+				"   	}                                        \n"
+				"   }                                            \n"
+				"}                                                                      \n"
+				"\n";
+
+const char *KernelSource_shared_memory =
 		"\n"
 				"__kernel void mymultiply(                                                 \n"
 				"   __global float* a,                                                  \n"
@@ -66,7 +102,7 @@ const char *KernelSource =
 
 //------------------------------------------------------------------------------
 
-int myopencl(float* h_a, float* h_b, float* h_c) {
+int myopencl(float* h_a, float* h_b, float* h_c, const char *KernelSource) {
 	int err;               // error code returned from OpenCL calls
 
 	int count = M_LENGTH;
@@ -83,11 +119,11 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 	cl_mem d_b;                    // device memory used for the input  b vector
 	cl_mem d_c;                    // device memory used for the output c vector
 
-	// Set up platform and GPU device
+// Set up platform and GPU device
 
 	cl_uint numPlatforms;
 
-	// Find number of platforms
+// Find number of platforms
 	err = clGetPlatformIDs(0, NULL, &numPlatforms);
 	checkError(err, "Finding platforms");
 	if (numPlatforms == 0) {
@@ -95,12 +131,12 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 		return EXIT_FAILURE;
 	}
 
-	// Get all platforms
+// Get all platforms
 	cl_platform_id Platform[numPlatforms];
 	err = clGetPlatformIDs(numPlatforms, Platform, NULL);
 	checkError(err, "Getting platforms");
 
-	// Secure a GPU
+// Secure a GPU
 	for (int i = 0; i < numPlatforms; i++) {
 		err = clGetDeviceIDs(Platform[i], DEVICE, 1, &device_id, NULL);
 		if (err == CL_SUCCESS) {
@@ -114,20 +150,21 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 	err = output_device_info(device_id);
 	checkError(err, "Printing device output");
 
-	// Create a compute context
+// Create a compute context
 	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
 	checkError(err, "Creating context");
 
-	// Create a command queue
+// Create a command queue
 	commands = clCreateCommandQueue(context, device_id, 0, &err);
 	checkError(err, "Creating command queue");
 
-	// Create the compute program from the source buffer
+// Create the compute program from the source buffer
 	program = clCreateProgramWithSource(context, 1,
-			(const char **) &KernelSource, NULL, &err);
+			(const char **) &KernelSource,
+			NULL, &err);
 	checkError(err, "Creating program");
 
-	// Build the program
+// Build the program
 	err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
 	if (err != CL_SUCCESS) {
 		size_t len;
@@ -141,13 +178,13 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 		return EXIT_FAILURE;
 	}
 
-	// Create the compute kernel from the program
+// Create the compute kernel from the program
 	ko_vadd = clCreateKernel(program, "mymultiply", &err);
 	checkError(err, "Creating kernel");
 
 	long matrix_size = sizeof(float) * count * count;
 
-	// Create the input (a, b) and output (c) arrays in device memory
+// Create the input (a, b) and output (c) arrays in device memory
 	d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, matrix_size, NULL, &err);
 	checkError(err, "Creating buffer d_a");
 
@@ -158,17 +195,17 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 	NULL, &err);
 	checkError(err, "Creating buffer d_c");
 
-	// Write a and b vectors into compute device memory
+// Write a and b vectors into compute device memory
 
 	err = clEnqueueWriteBuffer(commands, d_a, CL_TRUE, 0, matrix_size, h_a, 0,
-			NULL, NULL);
+	NULL, NULL);
 	checkError(err, "Copying h_a to device at d_a");
 
 	err = clEnqueueWriteBuffer(commands, d_b, CL_TRUE, 0, matrix_size, h_b, 0,
-			NULL, NULL);
+	NULL, NULL);
 	checkError(err, "Copying h_b to device at d_b");
 
-	// Set the arguments to our compute kernel
+// Set the arguments to our compute kernel
 	err = clSetKernelArg(ko_vadd, 0, sizeof(cl_mem), &d_a);
 	err |= clSetKernelArg(ko_vadd, 1, sizeof(cl_mem), &d_b);
 	err |= clSetKernelArg(ko_vadd, 2, sizeof(cl_mem), &d_c);
@@ -177,29 +214,29 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 
 	double rtime = wtime();
 
-	// Execute the kernel over the entire range of our 1d input data set
-	// letting the OpenCL runtime choose the work-group size
+// Execute the kernel over the entire range of our 1d input data set
+// letting the OpenCL runtime choose the work-group size
 	global = 64;
 	err = clEnqueueNDRangeKernel(commands, ko_vadd, 1, NULL, &global, NULL, 0,
 	NULL, NULL);
 	checkError(err, "Enqueueing kernel");
 
-	// Wait for the commands to complete before stopping the timer
+// Wait for the commands to complete before stopping the timer
 	err = clFinish(commands);
 	checkError(err, "Waiting for kernel to finish");
 
 	rtime = wtime() - rtime;
 	printf("\nThe kernel ran in %lf seconds\n", rtime);
 
-	// Read back the results from the compute device
+// Read back the results from the compute device
 	err = clEnqueueReadBuffer(commands, d_c, CL_TRUE, 0, matrix_size, h_c, 0,
-			NULL, NULL);
+	NULL, NULL);
 	if (err != CL_SUCCESS) {
 		printf("Error: Failed to read output array!\n%s\n", err_code(err));
 		exit(1);
 	}
 
-	// cleanup then shutdown
+// cleanup then shutdown
 	clReleaseMemObject(d_a);
 	clReleaseMemObject(d_b);
 	clReleaseMemObject(d_c);
@@ -210,4 +247,15 @@ int myopencl(float* h_a, float* h_b, float* h_c) {
 
 }
 
+void myopencl_simple(float* h_a, float* h_b, float* h_c) {
+	myopencl(h_a, h_b, h_c, KernelSource_simple);
+}
+
+void myopencl_temp_var(float* h_a, float* h_b, float* h_c) {
+	myopencl(h_a, h_b, h_c, KernelSource_temp_var);
+}
+
+void myopencl_shared_memory(float* h_a, float* h_b, float* h_c) {
+	myopencl(h_a, h_b, h_c, KernelSource_shared_memory);
+}
 #endif /* MOPENCL_H_ */
