f285e3e0463c869ff72f15a017e4e7a47666e2ee
Juan Sebastian Navarro <jsnavarroa@unal.edu.co>
2017-11-01 10:54:05 -0500

    Set the experiment

--- Exercise02/C/main.cpp
+++ Exercise02/C/main.cpp
@@ -15,12 +15,12 @@
 using namespace std;
 using namespace boost::timer;
 
-void printLog(long matrix_size, cpu_timer s_timer, cpu_timer o_timer, cpu_timer ocl_tv_timer, cpu_timer ocl_shm_timer,
-		bool test) {
-	string sequential = s_timer.format(3, "%ws");
-	string ocl_simple = o_timer.format(3, "%ws");
-	string ocl_tv = ocl_tv_timer.format(3, "%ws");
-	string ocl_shm = ocl_shm_timer.format(3, "%ws");
+void printLog(long matrix_size, cpu_timer s_timer, cpu_timer o_timer,
+		cpu_timer ocl_tv_timer, cpu_timer ocl_shm_timer, bool test) {
+	string sequential = s_timer.format(6, "%ws");
+	string ocl_simple = o_timer.format(6, "%ws");
+	string ocl_tv = ocl_tv_timer.format(6, "%ws");
+	string ocl_shm = ocl_shm_timer.format(6, "%ws");
 	string result = test ? "Passed" : "Failed";
 
 	printf("%ld \t %s \t %s \t", matrix_size, sequential.c_str(),
@@ -34,64 +34,66 @@ void printLog(long matrix_size, cpu_timer s_timer, cpu_timer o_timer, cpu_timer 
 
 int main(int argc, char *argv[]) {
 
-	M_LENGTH = 256;
+	for (int i = 2048; i <= 2048; i *= 2) {
 
-	bool result = true;
+		bool result = true;
 
-	cpu_timer sequential_timer;
-	cpu_timer opencl_timer, ocl_tv_timer, ocl_shm_timer;
+		M_LENGTH = i;
 
-	float* h_a = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float)); // a vector
-	float* h_b = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float)); // b vector
-	float* h_c = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float));
-	float* h_s = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float));
+		cpu_timer sequential_timer;
+		cpu_timer opencl_timer, ocl_tv_timer, ocl_shm_timer;
 
-	randomFill(h_a);
-	randomFill(h_b);
-	zeroFill(h_c);
-	zeroFill(h_s);
+		float* h_a = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float)); // a vector
+		float* h_b = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float)); // b vector
+		float* h_c = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float));
+		float* h_s = (float*) calloc(M_LENGTH * M_LENGTH, sizeof(float));
 
-	printf("hello 24 \n");
+		randomFill(h_a);
+		randomFill(h_b);
+		zeroFill(h_c);
+		zeroFill(h_s);
 
-	sequential_timer.start();
+		sequential_timer.start();
 
-	mymultiply(h_a, h_b, h_s);
+		mymultiply(h_a, h_b, h_s);
 
-	sequential_timer.stop();
-	//printMatrix((char *) "Sequential", h_s);
+		sequential_timer.stop();
+		//printMatrix((char *) "Sequential", h_s);
 
-	opencl_timer.start();
-	myopencl_simple(h_a, h_b, h_c);
-	opencl_timer.stop();
-	//printMatrix((char *) "OpenCL", h_c);
+		opencl_timer.start();
+		myopencl_simple(h_a, h_b, h_c);
+		opencl_timer.stop();
+		//printMatrix((char *) "OpenCL", h_c);
 
-	result = result && equal(h_s, h_c);
+		result = result && equal(h_s, h_c);
 
-	// OPENCL WITH TEMP VAR
-	zeroFill(h_c);
-	ocl_tv_timer.start();
-	myopencl_temp_var(h_a, h_b, h_c);
-	ocl_tv_timer.stop();
+		// OPENCL WITH TEMP VAR
+		zeroFill(h_c);
+		ocl_tv_timer.start();
+		myopencl_temp_var(h_a, h_b, h_c);
+		ocl_tv_timer.stop();
 
-	result = result && equal(h_s, h_c);
+		result = result && equal(h_s, h_c);
 
-	// OPENCL WITH shared memory
-	zeroFill(h_c);
-	ocl_shm_timer.start();
-	myopencl_shared_memory(h_a, h_b, h_c);
-	ocl_shm_timer.stop();
+		// OPENCL WITH shared memory
+		zeroFill(h_c);
+		ocl_shm_timer.start();
+		myopencl_shared_memory(h_a, h_b, h_c);
+		ocl_shm_timer.stop();
 
-	result = result && equal(h_s, h_c);
+		result = result && equal(h_s, h_c);
 
-	printf("m_size \t sequential \t ocl_simple \t oclTempVar \t oclSharedMem \t test \n");
-	printLog(M_LENGTH, sequential_timer, opencl_timer, ocl_tv_timer,
-			ocl_shm_timer, result);
+		printf(
+				"m_size \t sequential \t ocl_simple \t oclTempVar \t oclSharedMem \t test \n");
+		printLog(M_LENGTH, sequential_timer, opencl_timer, ocl_tv_timer,
+				ocl_shm_timer, result);
 
-	// FREE memory
+		// FREE memory
 
-	free(h_a);
-	free(h_b);
-	free(h_c);
+		free(h_a);
+		free(h_b);
+		free(h_c);
+	}
 
 	return 0;
 }
